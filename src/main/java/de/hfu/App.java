8bac633b56bbda3d3a861b6337c019096e95fb46
Katrin <katrin.arts@hs-furtwangen.de>
2020-11-13 13:46:05 +0100

    commit

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -9,9 +9,13 @@ import java.util.Scanner;
 public class App 
 {
     public static void main(String[] args) {
+    	/** Ausgabe, was der Nutzer eingeben soll. */
         System.out.println("Gib deinen Text ein");
+        /** scanner für die Eingabe eingefügt*/
         Scanner scanner = new Scanner(System.in);
+        /** Nutzereingabe wird eingelesen */
         String text = scanner.nextLine();
+        /** Nutzereingabe wird in Großbuchstaben ausgegeben */
         System.out.println(text.toUpperCase());
     }
 }
